252bfdd8f16625274fee1c414c482661fcfe0d0e
SANN GROUP <stokakunsanzz250@gmail.com>
2026-06-30 14:10:47 +0700

    Update metadata title and description for layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type {Metadata} from 'next';
 import './globals.css'; // Global styles
 
 export const metadata: Metadata = {
-  title: 'My Google AI Studio App',
-  description: 'My Google AI Studio App',
+  title: 'FOTO KITA BLUR',
+  description: 'FOTO KITA BLUR TREND TIKTOK',
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
